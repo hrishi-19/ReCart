0ba85d533e7e9c94deed290fc857daa950757699
hrishi-19 <hrishakamath.com>
2023-04-15 01:31:46 +0530

    layouting routes

--- client/src/Layout/RootLayout.jsx
+++ client/src/Layout/RootLayout.jsx
@@ -3,7 +3,12 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 import RegisterPage from '../Pages/registerPage'
 import LoginPage from '../Pages/loginPage'
-import HomeLayout from './homeLayout'
+import ProfilePage from '../Pages/profilePage'
+import AddProducts from '../Pages/addProducts'
+import MyProducts from '../Pages/myProducts'
+import Inbox from '../Pages/inbox'
+import HomePage from '../Pages/homePage'
+import ProductPage from '../Pages/productPage'
 
 function RootLayout() {
 
@@ -13,8 +18,15 @@ function RootLayout() {
             <Routes>
                 <Route path='/register' element={<RegisterPage />} />
                 <Route path='/login' element={<LoginPage />} />
-                <Route path='/home' element={<HomeLayout/>}/>
-            </Routes>   
+                <Route path='/home' >
+                    <Route path='' ele element={<HomePage />} />
+                    <Route path='profile' element={<ProfilePage />} />
+                    <Route path='add-product' element={<AddProducts />} />
+                    <Route path='my-products' element={<MyProducts />} />
+                    <Route path='inbox' element={<Inbox />} />
+                </Route>
+                <Route path='/product/:id' element={<ProductPage/>}/>
+            </Routes>
         </div>
     )
 }

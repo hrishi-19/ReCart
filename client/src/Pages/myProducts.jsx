42027a3bbfb857576e45336c4d12da3bba7778fb
hrishi-19 <hrishakamath.com>
2023-04-15 01:28:57 +0530

    data js

---
+++ client/src/Pages/myProducts.jsx
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+import SideBar from '../Components/sideBar'
+
+function MyProducts() {
+  return (
+    <Flex gap='10' p='10'>
+    <SideBar/>
+    myproducts
+    </Flex>
+  )
+}
+
+export default MyProducts
